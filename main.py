506a4aaa9fb21ce4723c5f38d85792951e9753db
SamraAzizi <samarraazizi@gmail.com>
2025-05-12 16:37:17 +0500

    simpleAiAgent

--- main.py
+++ main.py
@@ -23,4 +23,12 @@ def main():
         print("\nAssistant: ", end="")
         for chunk in agent_executor.stream(
             {"messages": [HumanMessages(content=user_input)]}
-        )
+        ):
+            if "agent" in chunk and "messages" in chunk['agent']:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+
+        print()
+
+if __name__=="__main__":
+    main()
